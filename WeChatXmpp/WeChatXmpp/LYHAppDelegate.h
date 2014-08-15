2fc73ad07eb564a9b850276a83c638c8d4eeb214
Charles Leo <liuyahuino1@sina.com>
2014-08-15 09:38:44 +0800

    第一次提交

---
+++ WeChatXmpp/WeChatXmpp/LYHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LYHAppDelegate.h
+//  WeChatXmpp
+//
+//  Created by Charles Leo on 14-8-15.
+//  Copyright (c) 2014年 Charles Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
